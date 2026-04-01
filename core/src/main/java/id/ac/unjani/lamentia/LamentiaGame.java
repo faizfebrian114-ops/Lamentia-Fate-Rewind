26a0e217fd0d3560c35e51df7793080d3d3cc08b
Faiz <email-github-kamu@example.com>
2026-04-01 21:45:05 +0700

    Menambahkan efek mengetik pada dialog

--- core/src/main/java/id/ac/unjani/lamentia/LamentiaGame.java
+++ core/src/main/java/id/ac/unjani/lamentia/LamentiaGame.java
@@ -1,32 +1,51 @@
 package id.ac.unjani.lamentia;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.ScreenUtils;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class LamentiaGame extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    SpriteBatch batch;
+    BitmapFont font;
+    
+    String fullText = "Faiz: 'Selamat datang di dunia Lamentia, tempat di mana takdir bisa ditulis ulang...'";
+    String currentDisplay = "";
+    float timer = 0;
+    int charIndex = 0;
 
     @Override
     public void create() {
         batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        font = new BitmapFont();
+        font.getData().setScale(2);
     }
 
     @Override
     public void render() {
         ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
+
+        // Logika Mengetik
+        if (charIndex < fullText.length()) {
+            timer += Gdx.graphics.getDeltaTime();
+            if (timer > 0.05f) { // Kecepatan ketik (makin kecil makin cepet)
+                currentDisplay += fullText.charAt(charIndex);
+                charIndex++;
+                timer = 0;
+            }
+        }
+
         batch.begin();
-        batch.draw(image, 140, 210);
+        font.setColor(Color.WHITE);
+        font.draw(batch, currentDisplay, 50, 200);
         batch.end();
     }
 
     @Override
     public void dispose() {
         batch.dispose();
-        image.dispose();
+        font.dispose();
     }
 }
